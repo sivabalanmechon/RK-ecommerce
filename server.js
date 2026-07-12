03e8d8f5cc8c1f5f4af4787aca7242c91865f70a
sivabalanmechon <sivabalan.arnpm@gmail.com>
2026-07-12 21:16:14 +0530

    new one

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
